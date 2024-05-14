917d243490c54b9000d479a3ec2199029416b622
raghveerdeol <deolraghveer5@gmail.com>
2024-05-14 11:35:24 +0200

    aggiunto createelemnt

---
+++ snack-8/js/script.js
@@ -0,0 +1,9 @@
+/* Snack8 
+Crea due tag div con due id diversi. 
+Un div avrà il testo colorato di rosso mentre l'altro di verde. 
+Partendo da un array di numeri, 
+stampiamo nell'id rosso i numeri dispari e in verde i numeri pari. */
+
+const divRed = document.createElement("div");
+const divGreen = document.createElement("div");
+
