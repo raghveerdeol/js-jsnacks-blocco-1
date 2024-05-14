c5cee6f1728c1f202af4a24e2b786002a56127ef
raghveerdeol <deolraghveer5@gmail.com>
2024-05-14 12:23:16 +0200

    aggiunto new element nel for e newArrey[index] nel if

--- snack-8/js/script.js
+++ snack-8/js/script.js
@@ -6,8 +6,19 @@ stampiamo nell'id rosso i numeri dispari e in verde i numeri pari. */
 
 const divRed = document.createElement("div");
 const divGreen = document.createElement("div");
-const main = document.querySelector("main")
-main.appendChild(divRed);
+const main = document.querySelector("main");
 main.appendChild(divGreen);
-divGreen.className = "green";
-divRed.className = "red";
+main.appendChild(divRed);
+divGreen.classList.add("green");
+divRed.classList.add("red");
+
+const newArray = [1,2,3,4,5,6,7,8,9];
+for (let index = 0; index < newArray.length; index++) {
+    const newElement = document.createElement("p");
+    newElement.append(newArray[index]);
+    if (newArray[index] % 2 === 0 ) {
+        divGreen.appendChild(newElement);
+    } else {
+        divRed.appendChild(newElement);
+}
+}
